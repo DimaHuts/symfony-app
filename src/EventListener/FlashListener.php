90061777dfdf74d781128369b59bbf26691804a5
Dima Huts <dima_ec_2011@mail.ru>
2018-01-16 15:30:05 +0300

    rename event subscriber dir

---
+++ src/EventListener/FlashListener.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace App\EventListener;
+
+
+use App\Events;
+use App\Service\CommonMessagesService;
+use Symfony\Component\EventDispatcher\Event;
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Symfony\Component\HttpFoundation\Session\Flash\FlashBagInterface;
+use Symfony\Component\Translation\TranslatorInterface;
+
+class FlashListener implements EventSubscriberInterface
+{
+
+    private $flashBag;
+    private $translator;
+    private $commonMessageService;
+
+    private static $messages = [
+        Events::PRODUCT_ADD => 'product.saved',
+        Events::SAVE_DB_ERROR => 'exception.save.db',
+        Events::PRODUCT_MODIFIED => 'product.modified',
+        Events::ENTITY_DOES_NOT_EXIST => 'entity.does.not.exist',
+        Events::PRODUCT_DELETED => 'product.deleted',
+        Events::USER_REGISTERED => 'user.registered',
+        Events::EMAIL_CONFIRMED => 'email.confirmed'
+    ];
+
+    /**
+     * FlashListener constructor.
+     *
+     * @param FlashBagInterface $flashBag
+     * @param TranslatorInterface $translator
+     * @param CommonMessagesService $commonMessagesService
+     */
+    public function __construct(FlashBagInterface $flashBag, TranslatorInterface $translator,
+                                CommonMessagesService $commonMessagesService)
+    {
+        $this->flashBag = $flashBag;
+        $this->translator = $translator;
+        $this->commonMessageService = $commonMessagesService;
+    }
+
+    public static function getSubscribedEvents()
+    {
+        return [
+            Events::PRODUCT_ADD => 'addSuccessMessage',
+            Events::SAVE_DB_ERROR => 'addDangerMessage',
+            Events::PRODUCT_MODIFIED => 'addSuccessMessage',
+            Events::ENTITY_DOES_NOT_EXIST => 'addDangerMessage',
+            Events::PRODUCT_DELETED => 'addSuccessMessage',
+            Events::USER_REGISTERED => 'addSuccessMessage',
+            Events::EMAIL_CONFIRMED => 'addSuccessMessage'
+        ];
+    }
+
+    public function addSuccessMessage($eventName)
+    {
+        $this->flashBag
+            ->add('success', $this->translator->trans(self::$messages[$eventName], [], 'messages'));
+    }
+
+    public function addDangerMessage($eventName)
+    {
+        $this->flashBag
+            ->add('danger', $this->translator->trans(self::$messages[$eventName], [], 'messages'));
+    }
+}
---
+++ src/EventListener/ProductListener.php
@@ -0,0 +1,23 @@
+<?php
+namespace App\EventListener;
+
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use App\Events;
+use App\Events\ProductEvent;
+
+class ProductListener implements EventSubscriberInterface
+{
+    public static function getSubscribedEvents()
+    {
+        return [
+            Events::PRODUCT_ADD => "onProductAdd",
+            Events::PRODUCT_MODIFIED => "onProductAdd"
+        ];
+    }
+    
+    public function onProductAdd(ProductEvent $event)
+    {
+        $event->getProduct()->setUser($event->getUser());
+    }
+
+}
